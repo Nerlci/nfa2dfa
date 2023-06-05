5526a5dc530fef892e6771a0ee8c7da544d85601
Nerlci_ <nerlci.underline@outlook.com>
2023-06-06 00:10:31 +0800

    fix wrong to_dfa method; implement to_networkx

--- automaton.py
+++ automaton.py
@@ -1,3 +1,7 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
 class State:
     def __init__(self, names=None):
         if names is None:
@@ -5,21 +9,32 @@ class State:
         if not isinstance(names, list):
             names = [names]
         self.names = names
+        self.num = 0
 
     def merge(self, y):
-        self.names = list(set(self.names) & set(y.names))
+        self.names = list(set(self.names) | set(y.names))
 
     def __str__(self):
         if len(self.names) == 1:
             return self.names[0]
-        ret_str = '['
-        for name in self.names:
-            ret_str += name + ', '
-        ret_str += ']'
-        return ret_str
+        self.names.sort()
+        return '[' + ', '.join(self.names) + ']'
+
+    def __eq__(self, other):
+        return len(self.names) == len(other.names) and len(self.names) == len(set(self.names) | set(other.names))
+
+    def __contains__(self, item):
+        return len(self.names) == len(set(self.names) | set(item.names))
+
+    def __len__(self):
+        return len(self.names)
+
+    def to_nx_node(self, is_final=False):
+        return self.num, {'names': self.names, 'name_str': self.__str__(),
+                          'final': is_final, 'initial': False}
 
 
-class Transfer:
+class Transition:
     def __init__(self, fr, to, char):
         if not isinstance(to, list):
             to = [to]
@@ -28,82 +43,134 @@ class Transfer:
         self.char = char
 
     def __str__(self):
-        return '({}, {} -> {})'.format(self.fr, self.char, self.to)
+        to_str = ', '.join(str(state) for state in self.to)
+        template = '({}, {} -> {})' if len(self.to) == 1 else '({}, {} -> {{{}}})'
+        return template.format(self.fr, self.char, to_str)
+
+    def to_nx_edge(self):
+        edges = []
+        for to in self.to:
+            edges.append((self.fr.num, to.num, {'char': self.char}))
+        return edges
 
 
-class Func:
-    def __init__(self, trans=None):
-        if trans is None:
-            trans = []
-        self.trans = trans
+class Function:
+    def __init__(self, transitions=None):
+        if transitions is None:
+            transitions = []
+        self.transitions = transitions
 
     def __iter__(self):
-        return self.trans.__iter__()
+        return self.transitions.__iter__()
+
+    def __getitem__(self, item):
+        return self.transitions[item]
+
+    def __setitem__(self, key, value):
+        self.transitions[key] = value
 
     def __str__(self):
-        ret_str = ''
-        for trans in self.trans:
-            ret_str += trans + ' | '
+        ret_str = ' | '.join(str(transition) for transition in self.transitions)
 
         return ret_str
 
-    def append(self, trans):
-        self.trans.append(trans)
+    def append(self, transition):
+        self.transitions.append(transition)
+
+    def to_nx_edges(self):
+        edges = []
+        for transition in self.transitions:
+            edges += transition.to_nx_edge()
+        return edges
 
 
 class Automaton:
-    def __init__(self, states=None, func=Func(), chars=None, init=None, final=None):
-        if chars is None:
-            chars = []
+    def __init__(self, states=None, function=Function(), characters=None, initial=None, final=None):
+        if characters is None:
+            characters = []
         if states is None:
             states = []
         if final is None:
             final = []
         self.states = states
-        self.func = func
-        self.init = init
+        self.function = function
+        self.initial = initial
         self.final = final
-        self.chars = chars
+        self.characters = characters
 
     def __str__(self):
-        states_str = '{'
-        for state in self.states:
-            states_str += state + ', '
-        states_str += '}'
+        states_str = '{' + ', '.join([str(state) for state in self.states]) + '}'
 
-        chars_str = '{'
-        for char in self.chars:
-            chars_str += char + ', '
-        chars_str += '}'
+        characters_str = '{' + ', '.join([str(char) for char in self.characters]) + '}'
 
-        final_str = '{'
-        for final in self.final:
-            final_str += final + ', '
-        final_str += '}'
+        final_str = '{' + ', '.join([str(final) for final in self.final]) + '}'
 
-        return '{}\n{}\n{}\n{}\n{}\n'.format(states_str, chars_str, self.func, self.init, final_str)
+        return '{}\n{}\n{}\n{}\n{}\n'.format(states_str, characters_str, self.function, self.initial, final_str)
+
+    def transition(self, state, char):
+        # Compute the next state(s) after transitioning with a character
+        next_state = State()
+
+        for transition in self.function:
+            if transition.fr in state and transition.char == char:
+                for state in transition.to:
+                    next_state.merge(state)
+
+        return next_state if len(next_state) > 0 else None
 
     def to_dfa(self):
-        new_states = self.states
-        new_func = Func()
-        new_final = []
-        flag = False
-        for trans in self.func:
-            if len(trans.to) > 1:
-                new_state = State()
-                for state in trans.to:
-                    new_state.merge(state)
-                    if state in self.final:
+        dfa_states = []
+        dfa_function = Function()
+        dfa_initial = State()
+        dfa_final = []
+
+        dfa_initial = self.initial
+
+        for state in self.states:
+            dfa_states.append(state)
+
+        for final in self.final:
+            dfa_final.append(final)
+
+        for state in dfa_states:
+            # 处理字母表中的每个字符
+            for char in self.characters:
+                # 获取使用字符进行转换后的下一个状态
+                next_state = self.transition(state, char)
+                # 如果转换后为空集，则不考虑
+                if next_state is None:
+                    continue
+                flag = False
+                for final in self.final:
+                    if final in next_state:
                         flag = True
 
-                if flag:
-                    new_final.append(new_state)
+                # 检查下一个状态是否已经存在于DFA中
+                if next_state not in dfa_states:
+                    dfa_states.append(next_state)
+                    if flag:
+                        dfa_final.append(next_state)
+                else:
+                    # 如果已经存在，则替换为现有的状态
+                    for state_ in dfa_states:
+                        if next_state == state_:
+                            next_state = state_
 
-                new_states.append(new_state)
-                new_func.append(Transfer(trans.fr, [new_state], trans.char))
-            else:
-                new_func.append(trans)
-        return Automaton(new_states, new_func, self.init, new_final)
+                transition = Transition(state, next_state, char)
+                dfa_function.append(transition)
+
+        # 创建新的自动机对象
+        dfa = Automaton(states=dfa_states, function=dfa_function, characters=self.characters,
+                        initial=dfa_initial, final=dfa_final)
+
+        return dfa
 
     def to_networkx(self):
-        pass
+        graph = nx.DiGraph()
+        for i in range(len(self.states)):
+            self.states[i].num = i
+        print(len(self.states))
+        graph.add_nodes_from([state.to_nx_node(is_final=state in self.final) for state in self.states])
+        graph.add_edges_from(self.function.to_nx_edges())
+        graph.nodes[self.initial.num]["initial"] = True
+        return graph
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import networkx as nx
 from matplotlib import pyplot as plt
-from automaton import State, Transfer, Func, Automaton
+from netgraph import Graph
+from automaton import State, Transition, Function, Automaton
 
 
 def get_states(state_str: str):
-    state_str = state_str.replace('{', '').replace('}', '').replace('}', '').split(',')
+    # 根据输入的状态字符串，构造状态对象
+    state_str = state_str.replace('{', '').replace('}', '').replace(' ', '').split(',')
     states = []
 
     for state in state_str:
@@ -14,6 +16,7 @@ def get_states(state_str: str):
 
 
 def get_named_state(name_str: str, states: list[State]):
+    # 根据名字查找状态
     for state in states:
         if len(state.names) > 1:
             continue
@@ -22,32 +25,87 @@ def get_named_state(name_str: str, states: list[State]):
 
 
 def get_chars(chars_str: str):
-    return chars_str.replace('{', '').replace('}', '').replace('}', '').split(',')
+    # 根据输入的字母表，构造字母表列表
+    return chars_str.replace('{', '').replace('}', '').replace(' ', '').split(',')
 
 
 def get_trans(trans_str: str, states):
-    head_str, to = trans_str.split('->')
+    # 根据转换字符串，构造转换对象
+    trans_str = trans_str.replace('(', '').replace(')', '')
+    head_str, to_str = trans_str.split('->')
+    to_str = to_str.replace('{', '').replace('}', '').replace(' ', '').split(',')
+    to_states = []
     fr, char = head_str.split(',')
-    fr, to, char = fr.strip(), to.strip(), char.strip()
-    fr, to = get_named_state(fr), get_named_state(to)
-    return Transfer(fr, to, char)
+    fr, char = fr.strip(), char.strip()
+    fr = get_named_state(fr, states)
+    for to in to_str:
+        to_states.append(get_named_state(to, states))
+    return Transition(fr, to_states, char)
 
 
 def get_func(func_str: str, states):
+    # 根据输入的转换字符串，构造转换函数对象
     func_str = func_str.split('|')
     trans = []
     for trans_str in func_str:
         trans_str = trans_str.strip()
         trans.append(get_trans(trans_str, states))
 
-    return Func(trans)
+    return Function(trans)
+
 
 def get_final(final_str, states):
-    state_str = state_str.replace('{', '').replace('}', '').replace('}', '').split(',')
+    # 根据输入的接受状态字符串，构造接受状态列表
+    final_str = final_str.replace('{', '').replace('}', '').replace(' ', '').split(',')
+    final_states = []
+    for final in final_str:
+        final_states.append(get_named_state(final, states))
+    return final_states
 
 
-state = get_states(input('Please enter state'))
-chars = get_chars(input('Please enter input characters'))
-func = get_func(input('Please enter transfer functions'), state)
-init = get_named_state(input('Please enter initial state'), state)
-final = get_final(input('Please enter final states'), state)
+def plot_graph(G):
+    # 将networkx图进行可视化绘制
+    pos = nx.spring_layout(G)
+    node_labels = nx.get_node_attributes(G, 'name_str')
+    node_final = nx.get_node_attributes(G, 'final')
+    node_initial = nx.get_node_attributes(G, 'initial')
+    edge_labels = nx.get_edge_attributes(G, 'char')
+
+    for key in node_final:
+        if node_final[key]:
+            node_final[key] = 3
+        else:
+            node_final[key] = 0.5
+
+    for key in node_initial:
+        if node_initial[key]:
+            node_initial[key] = 'b'
+        else:
+            node_initial[key] = 'black'
+
+    Graph(G, node_layout=pos, edge_layout='arc', origin=(-1, -1), scale=(3, 3),
+          node_size=15., node_labels=node_labels, node_label_fontdict=dict(size=10),
+          edge_labels=edge_labels, edge_label_fontdict=dict(size=12),
+          arrows=True, edge_layout_kwargs=dict(k=0.025), edge_width=4., node_edge_width=node_final,
+          node_edge_color=node_initial
+          )
+
+    plt.show()
+
+
+state = get_states(input(u'请输入状态: '))
+chars = get_chars(input(u'请输入输入字母表: '))
+func = get_func(input(u'请输入转移函数: '), state)
+init = get_named_state(input(u'请输入初始状态: '), state)
+final = get_final(input(u'请输入接受状态: '), state)
+
+automaton = Automaton(state, func, chars, init, final)
+
+dfa = automaton.to_dfa()
+
+print(u'转换后的DFA:')
+print(dfa)
+
+G = dfa.to_networkx()
+
+plot_graph(G)
